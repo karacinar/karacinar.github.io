9207793eae257e9177e1e12f40fccf708fc69359
Cinar Kara <c.kara@ogr.iu.edu.tr>
2025-03-02 23:24:44 +0300

    Converting Server Standard 2022 Evaluation version to Server Standard with retail key

--- PowerShell/Converting Server Standard 2022 Evaluation version to Server Standard with retail key.ps1
+++ PowerShell/Converting Server Standard 2022 Evaluation version to Server Standard with retail key.ps1
@@ -1,1 +1,17 @@
 dism /online /set-edition:ServerStandard /productkey:VDYBN-27WPP-V4HQT-9VMD4-VMK7H /accepteula
+
+# Windows Server 2025 Standard	TVRH6-WHNXV-R9WG3-9XRFY-MY832
+# Windows Server 2025 Datacenter	D764K-2NDRG-47T6Q-P8T8W-YP6DF
+# Windows Server 2025 Datacenter: Azure Edition	XGN3F-F394H-FD2MY-PP6FD-8MCRC
+
+# Windows Server 2022 Standard	VDYBN-27WPP-V4HQT-9VMD4-VMK7H
+# Windows Server 2022 Datacenter	WX4NM-KYWYW-QJJR4-XV3QB-6VM33
+# Windows Server 2022 Datacenter: Azure Edition	NTBV8-9K7Q8-V27C6-M2BTV-KHMXV
+
+# Windows Server 2019 Standard	N69G4-B89J2-4G8F4-WWYCC-J464C
+# Windows Server 2019 Datacenter	WMDGN-G9PQG-XVVXX-R3X43-63DFG
+# Windows Server 2019 Essentials	WVDHN-86M7X-466P6-VHXV7-YY726
+
+# Windows Server 2016 Standard	WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY
+# Windows Server 2016 Datacenter	CB7KF-BWN84-R7R2Y-793K2-8XDDG
+# Windows Server 2016 Essentials	JCKRF-N37P4-C2D82-9YXRT-4M63B
